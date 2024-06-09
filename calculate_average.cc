7a1559404a21d7e311fd35ac0f3aeb4a341ea284
Randy Zhao <zhaohongze@gmail.com>
2024-06-09 09:49:31 -0700

    Memory mapping works but very slow

--- calculate_average.cc
+++ calculate_average.cc
@@ -17,6 +17,7 @@ bool DEBUGGING = true;
 
 size_t CHUNK_SIZE = 1024 * 1024 * 50; // 50 MB chunks
 
+// std::string inputFileName = "./src/test/resources/samples/measurements-10.txt";
 std::string inputFileName = "./measurements.txt";
 
 int THREADS_COUNT = 5;
@@ -82,7 +83,9 @@ void output(Stations& stations) {
   std::cout << "}" << std::endl;
 }
 
-int fastS2I(char *data, int startIndex, int& result) {
+int fastS2I(const char *data, int startIndex, int& result) {
+  int temp10 = 0;
+
   int ptr = startIndex;
   bool isPos = true;
   if (data[ptr] == '+') {
@@ -97,49 +100,18 @@ int fastS2I(char *data, int startIndex, int& result) {
       continue;
     }
 
-    result = result * 10 + (data[ptr] - '0');
+    temp10 = temp10 * 10 + (data[ptr] - '0');
   }
 
-  if (isPos) {
-    result = -result;
-  }
+  result = isPos ? temp10 : -temp10;
 
   return ptr;
 }
 
 /**
- * Handle the chunk in data with range [startIdx, endIdx)
-*/
-void handleChunk(
-    char * data,
-    int startIdx,
-    int endIdx,
-    std::function<void(std::string&, int)> addMeasurement)
-{
-  int ptr = startIdx;
-  while (ptr < endIdx) {
-    // get name
-    std::string name;
-    char *nameStart = data + ptr;
-    for (;ptr < endIdx; ++ptr) {
-      if (data[ptr] == ';') {
-        name = std::string(nameStart, data + ptr);
-        break;
-      }
-    }
-
-    ++ptr; // consume ";"
-    int temperature10 = -1000;
-    ptr = fastS2I(data, ptr, temperature10);
-
-    addMeasurement(name, temperature10);
-  }
-}
-
-/**
  * Find the index of last \n in data, in the range of [startIdx, endIdx)
 */
-int findLastRowEnd(char* data, int startIdx, int endIdx) {
+int findLastRowEnd(const char* data, int startIdx, int endIdx) {
   int ptr = endIdx - 1;
   for (;ptr >= startIdx;--ptr) {
     if (data[ptr] == '\n') {
@@ -150,15 +122,114 @@ int findLastRowEnd(char* data, int startIdx, int endIdx) {
 }
 
 /**
+ * Similar to findLastRowEnd, but if the last row is incomplete, find its
+ * end without exceeding dataSize.
+*/
+int findLastRowEndExtended(const char* data, int startIdx, int endIdx, int dataSize) {
+  int lastRowEnd = findLastRowEnd(data, startIdx, endIdx);
+  if (lastRowEnd == endIdx - 1) {
+    return lastRowEnd;
+  }
+  int ptr = lastRowEnd + 1;
+  while (ptr < dataSize && data[ptr] != '\n') {
+    ++ptr;
+  }
+  return ptr < dataSize ? ptr : lastRowEnd;
+}
+
+/**
  * Find the index of first \n in data, in the range of [startIdx, endIdx)
 */
-int findFirstRowEnd(char* data, int startIdx, int endIdx) {
+int findFirstRowEnd(const char* data, int startIdx, int endIdx) {
   for (int ptr = startIdx; ptr < endIdx; ++ptr) {
     if (data[ptr] == '\n') {
       return ptr;
     }
   }
   return endIdx;
+}
+
+/**
+ * Handle the chunk in data with range [startIdx, endIdx).
+ * Skip the incomplete row at beginning (as it should be handled by the previous threads)
+ * but handle the incomplete row at the end.
+*/
+void handleChunk(
+    const char * data,
+    int startIdx,
+    int endIdx,
+    std::function<void(std::string&, int)> addMeasurement)
+{
+  int ptr = startIdx;
+
+  while (ptr < endIdx) {
+    // get name
+    std::string name;
+    const char *nameStart = data + ptr;
+    for (;ptr < endIdx; ++ptr) {
+      if (data[ptr] == ';') {
+        name = std::string(nameStart, data + ptr);
+        break;
+      }
+    }
+
+    ++ptr; // consume ";"
+    int temperature10 = -1000;
+    ptr = fastS2I(data, ptr, temperature10);
+    ++ptr; // consum "\n"
+
+    addMeasurement(name, temperature10);
+  }
+}
+
+void handleMappedMemoryWithThreads(
+    char *fileData,
+    size_t mapSize,
+    std::string& prevLeftOverData,
+    Stations& stations
+) {
+  auto addMeasurement = [&stations](std::string& name, int temperature10) {
+    std::lock_guard<std::mutex> lock(StationsMutex);
+    stations.try_emplace(name, Station());
+    stations[name].addMeasurement(temperature10);
+  };
+
+  int firstEndOfRow = -1;
+  if (prevLeftOverData.size() != 0) {
+    firstEndOfRow = findFirstRowEnd(fileData, 0, mapSize);
+    std::string row = prevLeftOverData + std::string(fileData, firstEndOfRow + 1);
+    handleChunk(row.c_str(), 0, row.length(), addMeasurement);
+    prevLeftOverData = "";
+  }
+
+  int lastEndOfRow = findLastRowEnd(fileData, 0, mapSize);
+  if (lastEndOfRow != mapSize - 1) {
+    prevLeftOverData = std::string(fileData + lastEndOfRow + 1, mapSize - lastEndOfRow - 1);
+  }
+
+  // std::vector<std::thread> threads;
+
+  // threads.emplace_back(
+  //   handleChunk,
+  //   fileData,
+  //   firstEndOfRow + 1,
+  //   mapSize,
+  //   mapSize,
+  //   addMeasurement
+  // );
+
+
+  // if (threads.size() >= THREADS_COUNT) {
+  //   for (auto& t: threads) {
+  //     t.join();
+  //   }
+  //   threads.clear();
+  // }
+
+  handleChunk(fileData, firstEndOfRow + 1, lastEndOfRow, addMeasurement);
+
+  // TODO: Handle first and last incomplete row
+
 }
 
 int main(int argc, char** argv) {
@@ -184,14 +255,10 @@ int main(int argc, char** argv) {
   size_t fileSize = sb.st_size;
   size_t ptr = 0;
 
-  char leftOverRowBuffer[LEFT_OVER_BUFFER_SIZE];
-  memset(leftOverRowBuffer, 0, sizeof(char) * LEFT_OVER_BUFFER_SIZE);
-  int leftOverRowBufferPtr = 0;
-
-  std::vector<std::thread> threads;
+  std::string leftOverData = "";
 
   while (ptr < fileSize) {
-    size_t mapSize = std::min(CHUNK_SIZE, fileSize - ptr);
+    size_t mapSize = std::min(CHUNK_SIZE * THREADS_COUNT, fileSize - ptr);
     char* fileData = (char*)mmap(nullptr, mapSize, PROT_READ, MAP_PRIVATE, fd, ptr);
     if (fileData == MAP_FAILED) {
       std::cerr << "Error mapping file!" << std::endl;
@@ -199,40 +266,7 @@ int main(int argc, char** argv) {
       return 1;
     }
 
-    int firstEndOfRow = findFirstRowEnd(fileData, 0, mapSize);
-    int lastEndOfRow = findLastRowEnd(fileData, 0, mapSize);
-
-    memcpy(leftOverRowBuffer + leftOverRowBufferPtr, fileData, firstEndOfRow);
-    leftOverRowBufferPtr += firstEndOfRow;
-
-    auto addMeasurement = [&stations](std::string& name, int temperature10) {
-      std::lock_guard<std::mutex> lock(StationsMutex);
-      stations.try_emplace(name, Station());
-      stations[name].addMeasurement(temperature10);
-    };
-
-    // TODO: unordered_map is not thread safe
-    threads.emplace_back(handleChunk, fileData, firstEndOfRow + 1, mapSize, addMeasurement);
-
-    if (leftOverRowBufferPtr >= HANDLE_LEFT_OVER_BUFFER_THREASHOLD) {
-      handleChunk(
-        leftOverRowBuffer,
-        0,
-        leftOverRowBufferPtr,
-        addMeasurement
-      );
-      memset(leftOverRowBuffer, 0, sizeof(char) * LEFT_OVER_BUFFER_SIZE);
-      leftOverRowBufferPtr = 0;
-    }
-    memcpy(leftOverRowBuffer + leftOverRowBufferPtr, fileData + lastEndOfRow + 1, mapSize - lastEndOfRow - 1);
-    leftOverRowBufferPtr += mapSize - lastEndOfRow - 1;
-
-    if (threads.size() >= THREADS_COUNT) {
-      for (auto& t: threads) {
-        t.join();
-      }
-      threads.clear();
-    }
+    handleMappedMemoryWithThreads(fileData, mapSize, leftOverData, stations);
 
     ptr += mapSize;
     munmap(fileData, mapSize);
